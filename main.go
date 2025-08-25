d69f31b661cbe5ceb215960b1deac9a5c1a6192c
xixotron <xixotron89@gmail.com>
2025-08-25 22:48:57 +0200

    single configuration point at the start of main

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 
 func main() {
 	const port = "8080"
+	const filePathRoot = "."
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
 	srv := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
