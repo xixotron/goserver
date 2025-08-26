f4d43d079080d2d68194d0cab2111c927d463577
xixotron <xixotron89@gmail.com>
2025-08-26 17:58:20 +0200

    /statistics, /reset and middlewareMetricsInc created and used to count the requests of /app

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
 	const filePathRoot = "."
 
+	appCfg := &apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
+	mux.Handle("/app/", http.StripPrefix("/app/", appCfg.middlewareMetricsInc(http.FileServer(http.Dir(filePathRoot)))))
 	mux.Handle("/healthz", handlerReadiness())
+	mux.Handle("/metrics", appCfg.handlerStatistics())
+	mux.Handle("/reset", appCfg.handlerReset())
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -30,5 +42,30 @@ func handlerReadiness() http.Handler {
 		w.Header().Add("Content-type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerStatistics() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hits: %v", cfg.fileServerHits.Load())
+	})
+}
+
+func (cfg *apiConfig) handlerReset() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Store(0)
+
+		w.Header().Add("Content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hits: %v", cfg.fileServerHits.Load())
 	})
 }
