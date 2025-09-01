ac47936e34ffd3cb4d944221ca8d06c1925b6134
xixotron <xixotron89@gmail.com>
2025-09-01 21:08:42 +0200

    chirp handlers split into different files

--- handle_chirps_post.go
+++ handle_chirps_post.go
@@ -20,53 +20,6 @@ type Chirp struct {
 	UserID    uuid.UUID `json:"user_id"`
 }
 
-func (cfg *apiConfig) handleGetChirps(w http.ResponseWriter, r *http.Request) {
-	response := []Chirp{}
-
-	chirps, err := cfg.db.GetAllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "unnable to get chirps", err)
-		return
-	}
-	for _, chirp := range chirps {
-		response = append(response, Chirp{
-			ID:        chirp.ID,
-			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,
-			Body:      chirp.Body,
-			UserID:    chirp.UserID,
-		})
-	}
-	respondWithJSON(w, http.StatusOK, response)
-}
-
-func (cfg *apiConfig) handleGetChirp(w http.ResponseWriter, r *http.Request) {
-	chirpIDstring := r.PathValue("chirpID")
-	chirpID, err := validateUUID(&chirpIDstring)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
-		return
-	}
-
-	chirp, err := cfg.db.GetChirpByID(r.Context(), chirpID)
-	if err != nil {
-		if err.Error() == "sql: no rows in result set" {
-			respondWithError(w, http.StatusNotFound, "couldn't get chirp", err)
-			return
-		}
-		respondWithError(w, http.StatusInternalServerError, "something went wrogn", err)
-		return
-	}
-
-	respondWithJSON(w, http.StatusOK, Chirp{
-		ID:        chirp.ID,
-		CreatedAt: chirp.CreatedAt,
-		UpdatedAt: chirp.UpdatedAt,
-		Body:      chirp.Body,
-		UserID:    chirp.UserID,
-	})
-}
-
 func (cfg *apiConfig) handlePostChirp(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body *string `json:"body"`
@@ -139,17 +92,6 @@ func validateChirp(chirp *string) (string, error) {
 	}
 
 	return replaceNotyWords(*chirp), nil
-}
-
-func validateUUID(id *string) (uuid.UUID, error) {
-	if id == nil {
-		return uuid.Nil, errors.New("no uuid provided")
-	}
-	resultID, err := uuid.Parse(*id)
-	if err != nil {
-		return uuid.Nil, errors.New("invalid user_id provided")
-	}
-	return resultID, nil
 }
 
 func replaceNotyWords(chirp string) string {
